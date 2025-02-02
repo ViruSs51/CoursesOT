85aa4c56d35748838b50aa14f8445f15d88bc938
Katsuryoku <vitalieberzoi555@gmail.com>
2025-02-02 17:43:29 +0200

    Finish init django project and Create and configure telegram bot

---
+++ Bot/main.py
@@ -0,0 +1,41 @@
+import asyncio
+import logging
+import os
+from pathlib import Path
+from dotenv import load_dotenv, set_key
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import WebAppInfo
+from aiogram.enums import ParseMode
+from pyngrok import ngrok
+
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+ENV_PATH = BASE_DIR / '.env'
+load_dotenv(ENV_PATH)
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+
+WEB_APP_URL = ngrok.connect(8000).public_url
+set_key(ENV_PATH, 'WEB_APP_URL', WEB_APP_URL)
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+@dp.message()
+async def start_command(message: types.Message):
+    web_button = types.InlineKeyboardButton(
+        text='🌐 Open Web App', web_app=WebAppInfo(url=WEB_APP_URL)
+    )
+    keyboard = types.InlineKeyboardMarkup(
+        inline_keyboard=[[web_button]]
+    )
+
+    await message.answer('Click the button for open web application:', reply_markup=keyboard)
+
+async def main():
+    logging.basicConfig(level=logging.INFO)
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
---
+++ README.md
@@ -0,0 +1,7 @@
+# Running 
+
+1. Start telegram bot:
+    ```python Bot/main.py```
+
+2. Start django web site:
+    ```python CoursesOT/manage.py runserver 0.0.0.0:8000```
