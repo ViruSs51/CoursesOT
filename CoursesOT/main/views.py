e7e6653a7f527f810fb7389b9e25326d79023d36
Katsuryoku <vitalieberzoi555@gmail.com>
2025-02-10 23:47:29 +0200

    Finish create tags list and categories list, update buttons template

--- CoursesOT/main/views.py
+++ CoursesOT/main/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-
-from .utils import load_svg
+from .utils import load_svg, get_tags
 
 # Create your views here.
 
 def home_page(request):
     data = {
-        'icons': load_svg()
+        'icons': load_svg(),
+        'tags': get_tags()
     }
 
     return render(request, 'main/home_page.html', data)
