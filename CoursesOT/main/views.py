64a311854d7d23b15f15494d624d39a615c491c0
Katsuryoku <vitalieberzoi555@gmail.com>
2025-02-10 19:59:48 +0200

    Finish create buttons template

--- CoursesOT/main/views.py
+++ CoursesOT/main/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from user_app.models import User
-from course.models import Course
+
+from .utils import load_svg
 
 # Create your views here.
-def home_page(request):
 
-    return render(request, 'main/home_page.html')
+def home_page(request):
+    data = {
+        'icons': load_svg()
+    }
+
+    return render(request, 'main/home_page.html', data)
